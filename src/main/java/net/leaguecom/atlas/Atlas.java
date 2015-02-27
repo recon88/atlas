5fd1b64799a3ee3c001b3d5010a6115b809734d1
AmShaegar <amshaegar@wrenchbox.de>
2015-02-28 00:08:08 +0100

    initial commit

---
+++ src/main/java/net/leaguecom/atlas/Atlas.java
@@ -0,0 +1,13 @@
+package net.leaguecom.atlas;
+
+/**
+ * Hello world!
+ *
+ */
+public class Atlas 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
